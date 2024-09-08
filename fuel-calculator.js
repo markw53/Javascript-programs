c6a2fd2fbd6a1b93da577c902dea683101b713e2
markw53 <m.workman@outlook.com>
2024-09-08 20:54:09 +0100

    add prompt command

--- fuel-calculator.js
+++ fuel-calculator.js
@@ -1,3 +1,5 @@
+const prompt=require("prompt-sync")({sigint:true}); 
+
 // Ask the user what type of fuel they use
 let fuelType = prompt("What type of fuel do you use? (e.g., Gasoline, Diesel, Electric)");
 
