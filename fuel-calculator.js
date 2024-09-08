db0651804aed0bf9108a56329437153b4bed55a2
markw53 <m.workman@outlook.com>
2024-09-08 17:37:57 +0100

    created fuel calculator

---
+++ fuel-calculator.js
@@ -0,0 +1,6 @@
+// Ask the user what type of fuel they use
+let fuelType = prompt("What type of fuel do you use? (e.g., Gasoline, Diesel, Electric)");
+
+// Output the result in the console
+console.log("You use: " + fuelType);
+
